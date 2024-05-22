214c85f0389a6500d38346a07e307548449076ee
Guishark <35200387+EchoesOfTheEye@users.noreply.github.com>
2024-05-22 20:12:38 +0100

    Added the parametric shapes

--- e2/js/main-script.js
+++ e2/js/main-script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
-
+import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js';
+import { ParametricGeometries } from 'three/addons/geometries/ParametricGeometries.js';
 var camera, scene, renderer;
 
 var materials = [
@@ -18,11 +19,77 @@ var materialsCilindro = [
     new THREE.MeshNormalMaterial({wireframe: false })
 ];
 
+var materialsPequeno = [];
+var materialsMedio = [];
+var materialsGrande = [];
+var objects = [];
 
 var carrossel, cilindroCentral, anelGrande, anelMedio, anelPequeno;
 
-var directionalLight, ambientLight;
+// ------------       parametric functions      ------------------- //
 
+const f1 = function (u, v, target) { // torus shape
+    var x = (6+Math.cos(u*2*Math.PI))*Math.cos(v*2*Math.PI);
+    var y = (3+Math.cos(u*2*Math.PI))*Math.sin(v*2*Math.PI);
+    var z = Math.sin(u*2*Math.PI);
+
+    target.set(x,y,z);
+};
+
+const f2 = function (u, v, target) { // crown
+
+    var x = Math.cos(u*v*2.5*Math.PI)*5;
+    var z = Math.sin(v*u*2.5*Math.PI)*5;
+    var y = Math.sin(v*u*20*Math.PI)*2;
+
+    target.set(x, y, z);
+};
+
+const f3 = function (u, v, target) { // twisty cone
+    var h = 8
+    var r = 3
+    var x = (h - u*h-3)*(r/h)*Math.cos(v*2*Math.PI);
+    var y = (h - u*h)*(r/h)*Math.sin(v*2*Math.PI);
+    var z = u*h;
+
+    target.set(x,y,z);
+}
+const f4 = function (u, v, target) { // bulb 
+    var r = 2+Math.sin(6*Math.PI*v);
+    var x = r*Math.cos(5*Math.PI*u)*Math.sin(4*Math.PI*v);
+    var y = r*Math.sin(5*Math.PI*u)*Math.sin(4*Math.PI*v);
+    var z = r*Math.cos(Math.PI*v);
+    target.set(x,y,z);
+}
+
+const f5 = function (u, v, target) { // the eye
+    var radius = 1; 
+    var twist = 2; 
+    var angle = u * Math.PI * 2 * twist;
+    var spiralFactor = 10; 
+    var spiralHeight = v * spiralFactor; 
+    var x = (radius * Math.cos(angle)) * Math.cos(v * Math.PI);
+    var y = (spiralHeight * Math.cos(angle)) * Math.sin(v * Math.PI);
+    var z = spiralHeight * Math.sin(angle);
+    target.set(x,y,z);
+}
+
+const f6 = function (u, v, target) { // tower of hanoi
+    var x = Math.sin(u*6*Math.PI) * (Math.cos(v*2*Math.PI) + 3);
+    var y = Math.cos(u*6*Math.PI) * (Math.cos(v*2*Math.PI) + 3);
+    var z = Math.sin(v*6*Math.PI) + Math.cos(v*2*Math.PI);
+    target.set(x, y, z);
+}
+
+const f7 = function (u, v, target) { // helix
+    var a = 2;
+    var x = v*2*Math.PI * Math.cos(u*4*Math.PI);
+    var z = v*2*Math.PI * Math.sin(u*4*Math.PI);
+    var y = a * u*2*Math.PI;
+    target.set(x, y, z);
+}
+
+// -----------------         parametric functions end       --------------- //
 
 class CustomCurve extends THREE.Curve {
 
@@ -54,6 +121,132 @@ class StraightLineCurve extends THREE.Curve {
 
         return optionalTarget.set(x, y, z);
     }
+}
+
+function addShapes(obj, r) {
+    var material = new THREE.MeshLambertMaterial( { color: 0xff00ff } );
+    materials.push(material);
+    
+
+    var geo1 = new ParametricGeometry( f1, 25, 25 );
+    var obj1 = new THREE.Mesh(geo1,material);
+    obj1.position.set(r,8,0);
+    var scalef = Math.random()*0.1+0.1
+    obj1.scale.set(scalef,scalef,scalef)
+    obj1.rotation.y = Math.random()*Math.PI*2
+    obj.add(obj1);
+    objects.push(obj1);
+    var sl1 = new THREE.SpotLight( 0xffffff );
+    sl1.intensity = 2;
+    sl1.angle = Math.PI/2
+    sl1.position.set(r, 5, 0)
+    sl1.target = obj1;
+    obj.add(sl1);
+
+    var geo2 = new ParametricGeometry( f2, 25, 25 );
+    var obj2 = new THREE.Mesh(geo2,material);
+    obj2.position.set(r*0.7,8,r*0.7);
+    var scalef = Math.random()*0.2+0.2
+    obj2.scale.set(scalef,scalef,scalef)
+    obj2.rotation.y = Math.random()*Math.PI*2
+    obj.add(obj2);
+    objects.push(obj2);
+    var sl2 = new THREE.SpotLight( 0xffffff );
+    sl2.intensity = 2;
+    sl2.angle = Math.PI/2
+    sl2.position.set(r*0.7, 5, r*0.7)
+    sl2.target = obj2;
+    obj.add(sl2);   
+    
+    var geo3 = new ParametricGeometry( f3, 25, 25 );
+    var obj3 = new THREE.Mesh(geo3,material);
+    obj3.position.set(0,8,r);
+    var scalef = Math.random()*0.2+0.2
+    obj3.scale.set(scalef,scalef,scalef)
+    obj3.rotation.y = Math.random()*Math.PI*2
+    obj.add(obj3);
+    objects.push(obj3);
+    var sl3 = new THREE.SpotLight( 0xffffff );
+    sl3.intensity = 2;
+    sl3.angle = Math.PI/2
+    sl3.position.set(0, 5, r)
+    sl3.target = obj3;
+    obj.add(sl3);    
+
+    var geo4 = new ParametricGeometry( f4, 25, 25 );
+    var obj4 = new THREE.Mesh(geo4,material);
+    obj4.position.set(-r*0.7,8,r*0.7);
+    var scalef = Math.random()*0.2+0.2
+    obj4.scale.set(scalef,scalef,scalef)
+    obj4.rotation.y = Math.random()*Math.PI*2
+    obj.add(obj4);
+    objects.push(obj4);
+    var sl4 = new THREE.SpotLight( 0xffffff );
+    sl4.intensity = 2;
+    sl4.angle = Math.PI/2
+    sl4.position.set(-r*0.7,5,r*0.7)
+    sl4.target = obj4;
+    obj.add(sl4);    
+
+    var geo5 = new ParametricGeometry( f5, 25, 25 );
+    var obj5 = new THREE.Mesh(geo5,material);
+    obj5.position.set(-r,8,0);
+    var scalef = Math.random()*0.2+0.2
+    obj5.scale.set(scalef,scalef,scalef)
+    obj5.rotation.y = Math.random()*Math.PI*2
+    obj.add(obj5);
+    objects.push(obj5);
+    var sl5 = new THREE.SpotLight( 0xffffff );
+    sl5.intensity = 2;
+    sl5.angle = Math.PI/2
+    sl5.position.set(-r,5,0)
+    sl5.target = obj5;
+    obj.add(sl5);    
+
+    var geo6 = new ParametricGeometry( f6, 25, 25 );
+    var obj6 = new THREE.Mesh(geo6,material);
+    obj6.position.set(-r*0.7,8,-r*0.7);
+    var scalef = Math.random()*0.2+0.2
+    obj6.scale.set(scalef,scalef,scalef)
+    obj6.rotation.y = Math.random()*Math.PI*2
+    obj.add(obj6);
+    objects.push(obj6);
+    var sl6 = new THREE.SpotLight( 0xffffff );
+    sl6.intensity = 2;
+    sl6.angle = Math.PI/2
+    sl6.position.set(-r*0.7,5,-r*0.7)
+    sl6.target = obj6;
+    obj.add(sl6);    
+
+    var geo7 = new ParametricGeometry( f7, 25, 25 );
+    var obj7 = new THREE.Mesh(geo7,material);
+    obj7.position.set(0,8,-r);
+    var scalef = Math.random()*0.2+0.2
+    obj7.scale.set(scalef,scalef,scalef)
+    obj7.rotation.y = Math.random()*Math.PI*2
+    obj.add(obj7);
+    objects.push(obj7);
+    var sl7 = new THREE.SpotLight( 0xffffff );
+    sl7.intensity = 2;
+    sl7.angle = Math.PI/2
+    sl7.position.set(0,5,-r)
+    sl7.target = obj7;
+    obj.add(sl7);    
+
+    var geo8 = new ParametricGeometry( ParametricGeometries.klein, 25, 25 );
+    var obj8 = new THREE.Mesh(geo8,material);
+    obj8.position.set(r*0.7,8,-r*0.7);
+    var scalef = Math.random()*0.2+0.1
+    obj8.scale.set(scalef,scalef,scalef)
+    obj8.rotation.y = Math.random()*Math.PI*2
+    obj.add(obj8);
+    objects.push(obj8);
+    var sl8 = new THREE.SpotLight( 0xffffff );
+    sl8.intensity = 2;
+    sl8.angle = Math.PI/2
+    sl8.position.set(r*0.7,5,-r*0.7)
+    sl8.target = obj8;
+    obj.add(sl8);    
 }
 
 function addAnelPequeno(obj, x, y, z) {
@@ -122,8 +315,8 @@ function createCarrossel(x, y, z) {
 
     addCilindroCentral(cilindroCentral, 0, 15, 0);
     addAnelGrande(anelGrande, 0, 3, 0);
-    addAnelMedio(anelMedio, 0, 9, 0); //6
-    addAnelPequeno(anelPequeno, 0, 15, 0); // 10
+    addAnelMedio(anelMedio, 0, 3, 0); //6
+    addAnelPequeno(anelPequeno, 0, 3, 0); // 10
 
     carrossel.add(cilindroCentral);
     carrossel.add(anelGrande);
@@ -161,6 +354,9 @@ function createScene() {
     scene.add(new THREE.AxesHelper(30));
 
     createCarrossel(0, 0, 0);
+    addShapes(anelGrande, 15);
+    addShapes(anelMedio, 10);
+    addShapes(anelPequeno, 5);
 }
 
 
@@ -170,9 +366,9 @@ function createCamera() {
                                          window.innerWidth / window.innerHeight,
                                          1,
                                          1000);
-    camera.position.x = 40;
-    camera.position.y = 40;
-    camera.position.z = 40;
+    camera.position.x = 20;
+    camera.position.y = 20;
+    camera.position.z = 0;
     camera.lookAt(scene.position);
 
 }
@@ -196,7 +392,7 @@ function onResize() {
 /////////////////////
 function createdirectionalLight() {
     'use strict';
-    directionalLight = new THREE.DirectionalLight( 0xffffff, 1);
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1);
     scene.add( directionalLight );
     directionalLight.position.set(1, 100, 200);
 }
@@ -204,7 +400,7 @@ function createdirectionalLight() {
 
 function createAmbientLight() {
     'use strict';
-    ambientLight = new THREE.AmbientLight(0xff7f00, 0.4); 
+    var ambientLight = new THREE.AmbientLight(0xff7f00, 0.4); 
     scene.add(ambientLight);
 }
 
